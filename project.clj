0be876cadb878a52056ee923029337c8af5f898e
Yaodan Zhang <ydzhang@thoughtworks.com>
2015-02-26 19:45:11 +0800

    [Yaodan] remove main method

--- project.clj
+++ project.clj
@@ -4,6 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :main ^:skip-aot sicp.in.clojure
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
