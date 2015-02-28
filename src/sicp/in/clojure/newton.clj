15e985d805eb695fdc2ae2ad3d8449dbb9ecb5bf
Yaodan Zhang <ydzhang@thoughtworks.com>
2015-02-28 10:23:49 +0800

    introduced square function

--- src/sicp/in/clojure/newton.clj
+++ src/sicp/in/clojure/newton.clj
@@ -1,23 +1,31 @@
 (ns sicp.in.clojure.newton
   (:gen-class))
 
-(defn improve
-  [input, guess]
-  (/ (+ (/ input guess) guess) 2.0))
+(defn average
+  [x, y]
+  (/ (+ x y) 2.0))
 
 (defn abs
   [x]
   (if (< x 0)
     (- 0 x) x))
 
+(defn square
+  [x]
+  (* x x))
+
+(defn improve
+  [input, guess]
+  (average (/ input guess) guess))
+
 (defn good-enough
-  [x, y]
-  (if (> (abs (- x y)) 0.000000001)
+  [input, guess]
+  (if (> (abs (- input (square guess))) 0.000000001)
     false true))
 
 (defn sqrt-with-guess
   [input, guess]
-  (if (good-enough (improve input guess) guess)
+  (if (good-enough input guess)
     guess (sqrt-with-guess input (improve input guess))))
 
 (defn sqrt
