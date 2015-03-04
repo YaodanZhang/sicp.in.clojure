db6e5aea1a95e8f93d607eb05aab745fcf47d98a
Yaodan Zhang <ydzhang@thoughtworks.com>
2015-03-04 20:49:23 +0800

    add rational

---
+++ src/sicp/in/clojure/rational.clj
@@ -0,0 +1,48 @@
+(ns sicp.in.clojure.rational
+  (:gen-class))
+
+(defn make-rat
+  [n, d]
+  (cons n [d]))
+
+(defn numer
+  [r]
+  (first r))
+
+(defn denom
+  [r]
+  (second r))
+
+(defn print-rat
+  [r]
+  (print (numer r) "/" (denom r))
+  r)
+
+(defn add-or-sub
+  [operation, x, y]
+  (make-rat (operation (* (numer x) (denom y))
+                       (* (denom x) (numer y)))
+            (* (denom x) (denom y))))
+
+(defn add
+  [x, y]
+  (add-or-sub + x y))
+
+(defn sub
+  [x, y]
+  (add-or-sub - x y))
+
+(defn mul
+  [x, y]
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(defn div
+  [x, y]
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(defn equal?
+  [x, y]
+  (= (* (numer x) (denom y))
+     (* (denom x) (numer y))))
