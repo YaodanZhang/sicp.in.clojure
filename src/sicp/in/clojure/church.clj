97661e39abd51844ce3f5f2dd931c1139b0434ed
Yaodan Zhang <ydzhang@thoughtworks.com>
2015-03-16 14:51:39 +0800

    changed the methods' order of church number

--- src/sicp/in/clojure/church.clj
+++ src/sicp/in/clojure/church.clj
@@ -1,12 +1,20 @@
 (ns sicp.in.clojure.church
   (:gen-class))
 
-(def church-0
-  (fn [f] (fn [x] x)))
-
 (defn church-add-1
   [n]
   (fn [f] (fn [x] (f ((n f) x)))))
+
+(defn show-church
+  [n]
+  ((n (fn [x] (+ x 1))) 0))
+
+(defn church-add
+  [a, b]
+  (fn [f] (fn [x] ((a f) ((b f) x)))))
+
+(def church-0
+  (fn [f] (fn [x] x)))
 
 (def church-1
   (fn [f] (fn [x] (f x))))
@@ -16,11 +24,3 @@
 
 (def church-10
   (fn [f] (fn [x] (f (f (f (f (f (f (f (f (f (f x)))))))))))))
-
-(defn show-church
-  [n]
-  ((n (fn [x] (+ x 1))) 0))
-
-(defn church-add
-  [a, b]
-  (fn [f] (fn [x] ((a f) ((b f) x)))))
